6c8fae923ae5930542140f23b87ba914883084ed
mendes-low <uskembaev07@icloud.com>
2023-11-02 16:18:14 +0600

    hw 02.11.23

---
+++ hw-02.11.23/lists.py
@@ -0,0 +1,160 @@
+# 1 level:
+
+# 1
+# lists = []
+
+# 2
+# cities = ['Almaty' , 'Astana' , 'Pavlodar' , 'Shymkent' , 'Semei']
+
+# 3
+# cities = ['Almaty' , 'Astana' , 'Pavlodar' , 'Shymkent' , 'Semei']
+# print(len(cities))
+
+# 4
+# cities = ['Almaty' , 'Astana' , 'Pavlodar' , 'Shymkent' , 'Semei']
+# print(cities[0])
+# print(cities[2])
+# print(cities[4])
+
+# 5
+# mixed_data_type = ['Aziz' , 16 , 178 , 'single' , 'Nurkent']
+# print(mixed_data_type)
+
+# 6
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print(it_companies)
+
+# 7
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print(it_companies)
+
+# 8
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print(len(it_companies))
+
+# 9
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print(it_companies[0])
+# print(it_companies[3])
+# print(it_companies[6])
+
+# 10
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# it_companies[5] = 'YouTube'
+# print(it_companies)
+
+# 11
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# it_companies.append('Dell')
+# print(it_companies)
+
+# 12
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# it_companies.insert(3 , 'Dell')
+# print(it_companies)
+
+# 13
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print(it_companies[5].upper())
+
+# 14
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print('\'#;'.join(it_companies))
+
+# 15
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print("Oracle" in it_companies)
+
+# 16
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# it_companies.sort()
+# print(it_companies)
+
+# 17
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# it_companies.reverse()
+# print(it_companies)
+
+# 18
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print(it_companies[:3])
+
+# 19
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print(it_companies[4:])
+
+# 20
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# print(it_companies[3:4])
+
+# 21
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# it_companies.pop(0)
+# print(it_companies)
+
+# 22
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# it_companies.pop(3)
+# print(it_companies)
+
+# 23
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# it_companies.pop(6)
+# print(it_companies)
+
+# 24
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# it_companies.clear()
+# print(it_companies)
+
+# 25
+# it_companies = ['Facebook' , 'Google' , 'Microsoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon']
+# del it_companies
+
+# 26
+# front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux'] 
+# back_end = ['Node','Express', 'MongoDB'] 
+# front_end.extend(back_end)
+# full_stack = front_end
+# print(full_stack)
+
+# 27
+# full_stack = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'Express', 'MongoDB']
+# full_stack.insert(5 , 'Python')
+# full_stack.insert(6 , 'SQL')
+# print(full_stack)
+
+# 2 level:
+
+# 1
+# ages = [19 , 22 , 19 , 24 , 20 , 25 , 26 , 24 , 25 , 24]
+# ages.sort()
+# print(ages)
+# min_age = min(ages)
+# max_age = max(ages)
+# print(min_age)
+# print(max_age)
+# ages.append(min_age)
+# ages.append(max_age)
+# print(ages)
+# print(ages[2] // 2)
+# sum_ages = sum(ages)
+# len_ages = len(ages)
+# midlle_age = sum_ages // len_ages
+# print(midlle_age)
+# print(max_age - min_age)
+# abs_max = abs(max_age and midlle_age)
+# print(abs_max)
+# abs_min = abs(min_age and midlle_age)
+# print(abs_min)
+
+# 3
+# contries = ['China' , 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+# contry1 , contry2 , contry3 , *other_contries = contries
+# print(contry1)
+# print(contry2)
+# print(contry3)
+# print(other_contries)
+
+
+
