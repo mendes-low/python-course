2436a0f9c1255af4c961838b8a217102a6c062e4
mendes-low <uskembaev07@icloud.com>
2023-11-09 17:00:12 +0600

    hw

---
+++ Python/set.py
@@ -0,0 +1,37 @@
+# cs = {'html' , 'css' , 'js' , 'ruby'}
+# cc = {'html' , 'css' , 'js' , 'ruby' , 'Java' , 'C#'}
+# sx = input('Lng:')
+# if sx in cs:
+#     print('Бар')
+# else:
+#     print('Жок')
+
+
+
+# person = input('Какой элемент добавить:')
+# if person in cs:
+#    print(f'Такой элемент уже есть:{person}')
+# else:
+#    cs.add(person)
+#    print(f'Мы добавили этот элементв список:{person}')
+#    print(cs)    
+
+# cs = {'html' , 'css' , 'js' , 'ruby'}
+# languages = input('Какой элемент добавить: ')
+# lang_list = languages.split(" ")
+# cs.update(lang_list)
+# print(cs)
+
+# cs = {'html' , 'css' , 'js' , 'ruby'}
+# langeage = input('Какой элемент удалить:')
+# if langeage in cs:
+    # cs.remove(langeage)
+    # print(f'Мы удалили:{langeage}')
+# else:
+    # print(f'Такого элемента нет в списке поэтому, мы удалили рандомный элемент:{cs.pop()}')
+    # print(cs)
+
+# cs = {'html' , 'css' , 'js' , 'ruby'}
+# cc = {'html' , 'css' , 'js' , 'ruby' , 'Java' , 'C#'}
+
+
